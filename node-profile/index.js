4d41787b6cd56c2d8c5515921fc06d7c20870a37
GalenWong <wonggalen1999@gmail.com>
2021-04-11 22:35:21 -0700

    added code snippet for profiling

---
+++ node-profile/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(express.json());
+
+const getCoinPrice = async coinId => {
+    const requestUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${coinId}&vs_currencies=usd`;
+    const resp = await fetch(requestUrl);
+    const data = await resp.json();
+    return data[coinId].usd;
+
+}
+
+app.get('/coin-prices', async (req, res) => {
+    const listOfCoins = ['dogecoin', 'bitcoin', 'ethereum', 'polkadot',
+        'litecoin', 'cardano', 'bitcoin-cash', 'stellar', 'chainlink',
+        'binancecoin'];
+
+    const prices = {};
+    for (const coinId of listOfCoins) {
+        prices[coinId] = await getCoinPrice(coinId);
+    }
+    res.json(prices);
+});
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+	console.log("listening on "+PORT);
+});
